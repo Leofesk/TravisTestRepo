75136e0b3c09c2133b98017a3b02f5127aff1085
Mikhail Gorelov <leofesk@gmail.com>
2018-10-05 23:05:23 +0300

    Added solution for testing TravisCI.

---
+++ TestSolutionForTravis/TestSolutionForTravis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSolutionForTravis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Travis");
+        }
+    }
+}
